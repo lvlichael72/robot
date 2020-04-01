d5385c422dc61e43c8553b0627f905d234f4dcf8
lvlichael72 <mahdi.moradi72@gmail.com>
2020-04-01 16:35:31 +0430

    added convertor to 2D list

--- robot2.py
+++ robot2.py
@@ -1,17 +1,34 @@
 import math
+from itertools import islice 
+
 
 #define n 
-n=6
+n=4
 
+#Create 2D  intitak List
 temp = [ 0 for x in range(n)]
-
 Matrix = [temp for x in range(n)]
+#print(Matrix)
 
-print(Matrix)
+# 1D to 2D list convertor
+def convert(lst, var_lst): 
+    it = iter(lst) 
+    return [list(islice(it, i)) for i in var_lst]
+
+
+# list convertor pattern init
+var_lst = [ n for x in range (n)]
+
+
+
+
+
+
 
 roadMap =[]
-pattern = []
 
+#init pattern
+pattern = []
 for x in range(n*n):
     pattern.append(0)
 
@@ -19,16 +36,18 @@ for x in range(n*n):
 
 #first and last step 
 step = (math.factorial((n-1)*2))/(math.factorial(n-1)*math.factorial(n-1))
-print(step)
+#print(step)
 pattern[0] = pattern [(n*n)-1] = step
 
+
+#OTHER steps
 for x in range(n):
     for y in range(n):
         if ((x==0 or y==0) and (x!=y)):
             step = math.factorial(2*n-2-x-y)/(math.factorial(n-1-x)*math.factorial(n-1-y))
             Matrix[x][y] = step 
             pattern [x*n + y]= step
-            print (x , 'x and y',y , 'step', step )
+            #print (x , 'x and y',y , 'step', step )
         if (((x==n-1) or (y==n-1) or (x>0 and y>0)) and not(x==y==n-1)):
             pass
             s = math.factorial(x+y)/(math.factorial(x)*math.factorial(y))
@@ -39,5 +58,6 @@ for x in range(n):
 
 
 
+Matrix = convert(pattern , var_lst)
 
-print(pattern)
+#print(Matrix[1][1])
