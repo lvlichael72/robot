a131d2fc8f1262cab0bc037c26251d4c68dd1128
lvlichael72 <mahdi.moradi72@gmail.com>
2020-04-03 19:14:54 +0430

    only works for n=4 and less still a lot of bugs!!!

--- robot2.py
+++ robot2.py
@@ -3,7 +3,7 @@ from itertools import islice
 
 
 #define n 
-n=4
+n=5
 
 #Create 2D  intitak List
 temp = [ 0 for x in range(n)]
@@ -72,14 +72,14 @@ print(roadMap)
 
 def CheckNext(x,y,direction):
 
-    if(direction == 'R'):
-        print(n*x+y+1,'pattern index')
+    if(direction == 'R' and (n*x+y+1) != n*n):
+        print(n*x+y+1,'pattern index','x=',x,'y=',y)
         
         if(pattern[n*x+y+1]!=0 and pattern[n*x+y+1] != -1):
             pattern[x*n+y] = pattern[x*n+y]-1
             return True
     elif(direction == 'U'):
-        print(pattern[n*x+y+1])
+        #print(pattern[n*x+y+1])
         if (pattern[n*x+y+n]!=0 and pattern[n*x+y+n] != -1 ):
             pattern[x*n+y] = pattern[x*n+y]-1
             return True
@@ -88,27 +88,33 @@ def CheckNext(x,y,direction):
             return False
         
     else:
-        return False
+        pattern[x*n+y] = pattern[x*n+y]-1
+        return True
 
 def CheckRepeat(tempRoad):
- 
-
-    if(len(memory) == 0): 
-        return True
-    elif (list(memory[len(memory)-1]) != tempRoad ):
-        return True
+    print('PATTERNG',pattern)
+    if (len(registered) != 0):
+        if(len(memory) == 0): 
+            return True
+        #elif (list(memory[len(memory)-1]) != tempRoad ):
+        elif (tuple(tempRoad) in list(memory)):
+                    print("CHECK REPEAT:",list(memory[len(memory)-1]) ,' and',tempRoad)
+                    return False
+        else:
+            print(memory[len(memory)-1],'memory')
+            print(tempRoad,'temproad')
+            print(registered,'regex')
+            print('$$$$$$$$')
+            return True
     else:
-        print(memory[len(memory)-1],'memory')
-        print(tempRoad,'temproad')
-        print(registered,'regex')
-        print('$$$$$$$$')
-        return False
+        return True
 
 
 def RightMove(x,y):
     print('right')
    # print(tempRoad)
     print(registered,'registerd')
+    print(pattern,'pattern FINAL')
     if(x==0 and y==0 and CheckNext(x,y,'R')): # (0,0) point
         tempRoad.append(RoadMapMatrix[x][y])
         print('001here')
@@ -139,7 +145,19 @@ def RightMove(x,y):
             #should call move again here !!!!!!!!!!!!!! otherwise its only one move
             RightMove(x,y)
         else:
-            pass
+            while(tempRoad[len(tempRoad)-1]-1 != tempRoad[len(tempRoad)-2]):
+                pattern[x*n+y]+=1
+                x-=1
+                tempRoad.pop()
+            
+
+            pattern[x*n+y]+=1
+            y-=1
+            tempRoad.pop()
+            UpMove(x,y)
+            print ('final position')
+            print('final temproad',tempRoad,'pattern',pattern)
+            print('register:',registered)
             #TODO: write the repeat ig
 
     if(x==n-2 and y==n-1):  #Pivot point 2 to register
@@ -177,7 +195,7 @@ def RightMove(x,y):
             if(CheckNext(x,y,'U')):
                 #pattern[x*n+y] = pattern[x*n+y]-1
                # tempRoad.append(RoadMapMatrix[x][y])
-                memory.append(tempRoad)
+                memory.append(tuple(tempRoad))
                 UpMove(x,y)
             else:
                 pattern[x*n+y] = 0
@@ -191,20 +209,43 @@ def RightMove(x,y):
             y-=1 # one step to left
             UpMove(x,y) # then goes up
 
-    if(x==n-1 and y<n-2):
+    if(x==n-1 and y<n-2 and y!=0):
         print('006here')
-        if(CheckNext(x,y,'R')):
+        
             #pattern[x*n+y] = pattern[x*n+y]-1
-            tempRoad.append(RoadMapMatrix[x][y])
-            y+=1
-            RightMove(x,y)
+        tempRoad.append(RoadMapMatrix[x][y])
+        if(CheckRepeat(tempRoad)):
 
+            if(CheckNext(x,y,'R')):
+                memory.append(tuple(tempRoad))
+                y+=1
+            #print('problemo')
+                RightMove(x,y)
+
+            else:
+                #TODO this is wrong and should be changed
+                #print('problemo')
+                pattern[x*n+y] = 0 
+                tempRoad.clear()
+                x=0
+                y=0
+                RightMove(x,y)
+        
         else:
-            pattern[x*n+y] = 0 
-            tempRoad.clear()
-            x=0
-            y=0
-            RightMove(x,y)
+            while(tempRoad[len(tempRoad)-1]-1 != tempRoad[len(tempRoad)-2]):
+                pattern[x*n+y]+=1
+                x-=1
+                tempRoad.pop()
+            
+
+            pattern[x*n+y]+=1
+            y-=1
+            tempRoad.pop()
+            UpMove(x,y)
+           
+
+
+
     if(x==0 and y==n-1):                            #this is for corner right down
         if (CheckNext(x,y,'U')):
             tempRoad.append(RoadMapMatrix[x][y])
@@ -212,6 +253,14 @@ def RightMove(x,y):
             UpMove(x,y)
         else:
             pattern[n*x+y] = 0 
+    if(y==0 and x==n-1):
+        if(CheckNext(x,y,'R')):
+            tempRoad.append(RoadMapMatrix[x][y])
+            print('Check corner',x , y)
+            y+=1
+            RightMove(x,y)
+        else:
+            pattern[n*x+y]=0
             
     else:
         if(CheckNext(x,y,'R')):
